2d0056139a7008baf191651e7cb54a5732e3379e
Animesh Kumar <animesh@Animeshs-MacBook-Air.local>
2023-09-29 15:14:38 +0530

    fix registration login

--- index.js
+++ index.js
@@ -3,84 +3,115 @@ const path = require("path");
 const app = express();
 const hbs = require("hbs");
 const bcrypt = require('bcrypt');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const Register = require("./models/register"); 
-const PORT = process.env.PORT || 3300
+const User = require("./models/register");
+const PORT = process.env.PORT || 3300;
 const connectDB = require("./db/conn");
 
-
+// Require the Register model here
+// const Register = require('./models/register');
 
 const static_path = path.join(__dirname, 'public');
 const template_path = path.join(__dirname, 'templates', 'views');
 const partials_path = path.join(__dirname, 'templates', 'partials');
 
-
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(static_path));
-app.set("view engine","hbs");
-app.set("views",template_path);
+app.set("view engine", "hbs");
+app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
-
+app.get(["/", "/home"], (req, res) => {
+  const isAuthenticated = req.isAuthenticated;
+  res.render("index", { isAuthenticated });
+});
 
 app.get('/register', (req, res) => {
-    res.render('register'); 
+  res.render('register');
 });
 
-app.post('/register', async (req, res) => {
-    try {
-        const {
-            firstName,
-            lastName,
-            email,
-            mobileNumber,
-            adhaarCardNumber,
-            dateOfBirth,
-            userType,
-            password,
-            confirmPassword
-        } = req.body;
 
-       
-        if (password !== confirmPassword) {
-            return res.status(400).send("Passwords do not match.");
-        }
 
-       
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-      
-        const newUser = new Register({
-            firstName,
-            lastName,
-            email,
-            mobileNumber,
-            adhaarCardNumber,
-            dateOfBirth,
-            userType,
-            password: hashedPassword,  
-            confirmPassword: hashedPassword, 
-        });
-      
-        // console.log(newUser);
-
-        const savedUser = await newUser.save();
-        //  console.log(savedUser);
-
-        res.status(201).send("Registration successful!");
-    } catch (error) {
-        console.error("Registration error:", error);
-        res.status(500).send(`Registration failed. Error: ${error.message}`);
-    }
-});
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is Running at http://localhost:${PORT}`);
+app.post("/register", async (req, res) => {
+  //console.log(req.body);
+  try {
+    const registerEmployee = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      mobileNumber: req.body.mobileNumber,
+      gender: req.body.gender,
+      age: req.body.age,
+      category: req.body.category,
+      adhaarCardNumber: req.body.adhaarCardNumber,
+      password: req.body.password,
     });
 
-})
+    const registered = await registerEmployee.save();
+
+  
+    const token = await registerEmployee.generateAuthToken();
+
+
+    res.cookie("jwt", token, {
+      expires: new Date(Date.now() + 30000000),
+      httpOnly: true
+    });
+
+    res.render("register", { success: "Registration successful!" });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(400).render("register", { error: "Registration failed." });
+  }
+});
+
+
+app.get('/login', (req, res) => {
+  const isAuthenticated = req.isAuthenticated; 
+  res.render('login', { isAuthenticated });
+});
+
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      
+      return res.redirect("/login?error=Invalid%20email%20or%20password");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      // Successful login
+      const token = await user.generateAuthToken();
+
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 30000000),
+        httpOnly: true,
+      });
+
+      return res.redirect("/");
+    } else {
+      // Handle case where the password doesn't match
+      return res.redirect("/login?error=Invalid%20email%20or%20password");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is Running at http://localhost:${PORT}`);
+  });
+});
--- models/register.js
+++ models/register.js
@@ -1,84 +1,81 @@
-const async = require("hbs/lib/async");
 const mongoose = require("mongoose");
-const bcrypt =require("bcrypt");
-const jwt=require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const employeeSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    mobileNumber: {
-        type: Number,
-        required: true
-    },
-    adhaarCardNumber: {
-        type: Number,
-        required: true
-    },
-    dateOfBirth: {
-        type: Date,
-        required: true
-    },
-    userType: {
-        type: Number,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    confirmPassword: {
-        type: String,
-        required: true
-    },
-    cart: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Cart'
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
-})
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobileNumber: {
+    type: String, // Assuming the phone number can contain non-numeric characters
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number, // You can adjust the data type as needed
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  adhaarCardNumber: {
+    type: String, // Assuming Aadhar numbers can contain non-numeric characters
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-employeeSchema.methods.generateAuthToken = async function(){
-    try {
-  
-        const token = jwt.sign({_id:this._id.toString()},'grindtillyoufckit/reversethekey/asdsad');
-        this.tokens = this.tokens.concat({token:token});
+userSchema.methods.generateAuthToken = async function(){
+  try {
+ //     console.log(this._id);
 
-        await this.save();
-        return token;
+      const token = jwt.sign({_id:this._id.toString()},'grindtillyoufckit/reversethekey/asdsad');
+      this.tokens = this.tokens.concat({token:token});
 
-    } catch (error) {
-     
-        console.log("The Error part "+ error);
-    }
+//     console.log(token);
+
+      await this.save();
+      return token;
+
+  } catch (error) {
+      //res.send("The Error part " + error);
+      console.log("The Error part "+ error);
+  }
 }
 
-employeeSchema.pre("save",async function(next){
+
+userSchema.pre("save",async function(next){
 
      
-    if(this.isModified("password"))
-    {
-       // console.log(`The current password ${this.password}`);
-        this.password = await bcrypt.hash(this.password,10)
-       // console.log(`The current password ${this.password}`);
-    }
-    next();
+  if(this.isModified("password"))
+  {
+     // console.log(`The current password ${this.password}`);
+      this.password = await bcrypt.hash(this.password,10)
+     // console.log(`The current password ${this.password}`);
+  }
+  next();
 })
 
-const Register = new mongoose.model("user",employeeSchema);
-module.exports=Register;
+
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
